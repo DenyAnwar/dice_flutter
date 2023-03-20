2e1f6ef5ad18a05232dfc041cc676320620d3116
denyanwardev <denyanwardev@gmail.com>
2023-03-20 12:13:01 +0800

    add functionality of dice when it pressed

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.teal[400],
         appBar: AppBar(
           title: Text('Dice'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.teal[400],
         ),
         body: DicePage(),
       ),
@@ -22,6 +23,15 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -32,10 +42,7 @@ class _DicePageState extends State<DicePage> {
               padding: const EdgeInsets.all(16.0),
               child: TextButton(
                 onPressed: () {
-                  setState(() {
-                    leftDiceNumber = 5;
-                    print('diceNumber = $leftDiceNumber');
-                  });
+                  changeDiceFace();
                 },
                 child: Image.asset('images/dice$leftDiceNumber.png'),
               ),
@@ -46,9 +53,9 @@ class _DicePageState extends State<DicePage> {
               padding: const EdgeInsets.all(16.0),
               child: TextButton(
                 onPressed: () {
-                  print('Right button got pressed');
+                  changeDiceFace();
                 },
-                child: Image.asset('images/dice1.png'),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
               ),
             ),
           ),
